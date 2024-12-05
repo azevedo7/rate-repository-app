9623ec63074750059f62a450d28b0e9e567ec4f7
joao azevedo <jazevedo0406@gmail.com>
2024-12-05 18:34:47 +0000

    Exercise 10.4

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -5,11 +5,17 @@ const styles = StyleSheet.create({
     container: {
         paddingTop: Constants.statusBarHeight,
         backgroundColor: '#24292e'
+    },
+    containerText: {
+        color: '#FFFFFF',
+        fontSize: 20,
+        fontWeight: '600',
+        padding: 20,
     }
 })
 
 const AppBar = () => {
-    return <View style={styles.container}><Text>Repositories</Text></View>
+    return <View style={styles.container}><Text style={styles.containerText}>Repositories</Text></View>
 }
 
 export default AppBar
